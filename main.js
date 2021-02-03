2eb228e23a940fc0ec84e9eb777fc8207f970ec2
Ivan <ivanchernetskey@mail.ru>
2021-02-03 20:48:50 +0300

    yeeeee i fixed bug and can use buttons without refresh page and it works

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const usersURL = 'https://jsonplaceholder.typicode.com/users'
 
+let FLAG = 0
+
 let baseUsernames = []
 
 function requestUsers(){
@@ -51,87 +53,96 @@ name1.onclick = () => {
     }
 }
 
-//переменная в которой будет храниться результат поиска либо его отсутсвие
-let find_result
+function clickOnButtonFind() {
+    //обрабатываем нажатие на кнопку
+    document.querySelector('.find_button').addEventListener('click',(e) => {
 
-//обрабатываем нажатие на кнопку
-document.querySelector('.find_button').addEventListener('click',(e) => {
+        let textFind = document.querySelector('.field').value
 
-    let textFind = document.querySelector('.field').value
+        //избегаю ситуации случайного нажатия на Find с пустым
+        // запросом(чтобы не показало что ничег не найдено, а ведь никто и не искал)
+        if(textFind !== "") {
 
-    //избегаю ситуации случайного нажатия на Find с пустым
-    // запросом(чтобы не показало что ничег не найдено, а ведь никто и не искал)
-    if(textFind !== "") {
-        //фильтрую базу по условию идентичности введенного с тем что в массиве
-        find_result = baseUsernames.filter((item) => {
-            return item.toLowerCase() == textFind.toLowerCase()
-        })
+            let nodes = document.querySelectorAll('.container__item')
 
-        let nodes = document.querySelectorAll('.container__item')
+            nodes.forEach( (item) => {
+                //после долгих попыток все таки получил, с моей точки зрения, эффективный метод удаления узлов
+                if(item.innerText.toLowerCase().search(textFind.toLowerCase()) === -1){
+                    item.remove()
+                }
+            })
 
-        nodes.forEach( (item) => {
-            //после долгих попыток все таки получил, с моей точки зрения, эффективный метод удаления узлов
-            if(item.innerText.toLowerCase().search(textFind.toLowerCase()) === -1){
-                item.remove()
+            //На случай того что ничего не найдено
+            const spaceAfterSearch = document.querySelector('.container__item')
+
+            if(spaceAfterSearch === null){
+
+                let weDontFind = document.querySelector('.container')
+
+                weDontFind.insertAdjacentHTML('afterbegin',
+                    '<div class="notFoundText">\n' +
+                    '  <div style="text-align: center; font-size: 20px; font-weight: bold; font-style: italic; margin-top: 20px;">\n' +
+                    'К великому сожалению мы ничего не нашли</div>\n' +
+                    '\t<div style="margin-left:auto;margin-right:auto;margin-top: 20px;width: fit-content;">\n' +
+                    '                <img src="https://i.gifer.com/4qb.gif" />\n' +
+                    '\t</div>\n' +
+                    '</div>'
+                )
+
+                //удаляю кнопку поиска чтобы исбежать повторной обработки этого же события и появления
+                //нескольких кнопок Esc
+                document.querySelector('.find_button').remove()
+
+                //после удаления кнопки ОПУСКАЮ флаг (0)
+                FLAG--
+
+                let weWantBack = document.querySelector('.header')
+
+                weWantBack.insertAdjacentHTML('beforeend','<button type="submit" class="esc_button">Esc</button>')
             }
-        })
-
-        //На случай того что ничего не найдено
-        let spaceAfterSearch = document.querySelector('.container__item')
-
-        if(spaceAfterSearch === null){
-
-            let weDontFind = document.querySelector('.container')
-
-            weDontFind.insertAdjacentHTML('afterbegin',
-                '<div class="notFoundText">\n' +
-                '  <div style="text-align: center; font-size: 20px; font-weight: bold; font-style: italic; margin-top: 20px;">\n' +
-                'К великому сожалению мы ничего не нашли</div>\n' +
-                '\t<div style="margin-left:auto;margin-right:auto;margin-top: 20px;width: fit-content;">\n' +
-                '                <img src="https://i.gifer.com/4qb.gif" />\n' +
-                '\t</div>\n' +
-                '</div>'
-            )
-
-            //удаляю кнопку поиска чтобы исбежать повторной обработки этого же события и появления
-            //нескольких кнопок Esc
-            document.querySelector('.find_button').remove()
-
-            let weWantBack = document.querySelector('.header')
-
-            weWantBack.insertAdjacentHTML('beforeend','<button type="submit" class="esc_button">Esc</button>')
+        } else{
+            alert('Вы еще ничего не ввели!')
         }
-    } else{
-        alert('Вы еще ничего не ввели!')
-    }
-})
+    })
+}
 
-//установил интервал для поиска момента когда появиться кнопка
-//а затем добаляю ей слушателя
+function clickOnButtonEsc() {
+
+    //добавляем слушателя события нажатия на кнопку Esc
+    document.querySelector('.esc_button').addEventListener('click', (event) => {
+        //удаляю Криминальное чтиво
+        document.querySelector('.notFoundText').remove()
+
+        //Удаляю содержимое поля
+        document.querySelector('.field').value = ''
+
+        //удаляю кнопку Esc
+        document.querySelector('.esc_button').remove()
+        //после удаления кнопки ОПУСКАЮ флаг (0)
+        FLAG--
+
+        //запрашиваю пользователей
+        requestUsers()
+
+        //добавляю кнопку поиска
+        document.querySelector('.header').insertAdjacentHTML('beforeend',
+            '<button type="submit" class="find_button">Find</button>')
+    })
+}
+
+//ОЧЕНЬ ВАЖНО!!!!
+//установил интервал для поиска момента когда появяться кнопки
+//а затем добаляю им слушателя и глвное условие по одному разу
 let findEscInterval = setInterval( () => {
-    if (document.querySelector('.esc_button') !== null) {
-
-        //прерываю интервал как только нашел кнопку esc
-        //и выполнить то что в call-back функции
-        clearInterval(findEscInterval)
-
-        //добавляем слушателя события нажатия на кнопку Esc
-        document.querySelector('.esc_button').addEventListener('click', (event) => {
-            //удаляю Криминальное чтиво
-            document.querySelector('.notFoundText').remove()
-
-            //Удаляю содержимое поля
-            document.querySelector('.field').value = ''
-
-            //удаляю кнопку Esc
-            document.querySelector('.esc_button').remove()
-
-            //запрашиваю пользователей
-            requestUsers()
-
-            //добавляю кнопку поиска
-            document.querySelector('.header').insertAdjacentHTML('beforeend',
-                '<button type="submit" class="find_button">Find</button>')
-        })
+    if (document.querySelector('.esc_button') !== null && FLAG === 0) {
+        //НАЖАЛ НА КНОПКУ - ПОДНЯЛ ФЛАГ
+        FLAG++
+        clickOnButtonEsc()
     }
+    if(document.querySelector('.find_button') !== null && FLAG === 0){
+        //НАЖАЛ НА КНОПКУ - ПОДНЯЛ ФЛАГ
+        FLAG++
+        clickOnButtonFind()
+    }
+
 }, 500)
